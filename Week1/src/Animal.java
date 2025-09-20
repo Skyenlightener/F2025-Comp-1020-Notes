b5bffc73105befde7b7aa4095b53b17b643f87f4
Skyenlightener <dw5326771@gmail.com>
2025-09-20 06:22:13 -0500

    update

---
+++ Week1/src/Animal.java
@@ -0,0 +1,9 @@
+public class Animal {
+    private String name;
+    private int numLegs;
+
+    public Animal(String name; int numLegs){
+        this.name = name;
+        this.numLegs = numLegs;
+    }
+}
